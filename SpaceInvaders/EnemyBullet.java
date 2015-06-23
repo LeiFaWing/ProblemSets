ee89313a4a17b9f6eda88a437ff954a8832f2005
LeiFaWing <stevehlee@sbcglobal.net>
2015-06-23 15:40:10 -0700

    Create EnemyBullet.java

---
+++ SpaceInvaders/EnemyBullet.java
@@ -0,0 +1,25 @@
+package SpaceInvaders;
+
+public class EnemyBullet {
+	int x;
+	int y;
+	int width;
+	int height;
+	
+	public EnemyBullet() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public boolean checkCollision() {
+		
+		return false;
+	}
+}
