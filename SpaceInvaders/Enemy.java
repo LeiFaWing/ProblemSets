0bb7eff44ab8f201f8bacf7e326a297261e671e6
LeiFaWing <stevehlee@sbcglobal.net>
2015-06-23 15:39:25 -0700

    Create Enemy.java

---
+++ SpaceInvaders/Enemy.java
@@ -0,0 +1,26 @@
+package SpaceInvaders;
+
+public class Enemy {
+	int x;
+	int y;
+	int sides;
+	int speed;
+	
+	public Enemy() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public boolean shoot() {
+		
+		
+		return false;
+	}
+}
