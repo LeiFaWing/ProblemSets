c06cc3c4f0fef57ac7eccac6b5a316478256fe7d
LeiFaWing <stevehlee@sbcglobal.net>
2015-06-23 15:39:50 -0700

    Create PlayerBullet.java

---
+++ SpaceInvaders/PlayerBullet.java
@@ -0,0 +1,25 @@
+package SpaceInvaders;
+
+public class PlayerBullet {
+	int x;
+	int y;
+	int width;
+	int height;
+	
+	public PlayerBullet() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public boolean checkCollision() {
+		
+		return false;
+	}
+}
