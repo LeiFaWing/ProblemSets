4e6e466f55072cef8cdb104ace70c8ffb12b6723
LeiFaWing <stevehlee@sbcglobal.net>
2015-06-24 14:26:12 -0700

    Update EnemyBullet.java

--- SpaceInvaders/EnemyBullet.java
+++ SpaceInvaders/EnemyBullet.java
@@ -1,24 +1,47 @@
 package SpaceInvaders;
 
+import apcs.Window;
+
 public class EnemyBullet {
 	int x;
 	int y;
 	int width;
 	int height;
+	int speed;
 	
-	public EnemyBullet() {
-		
+	public EnemyBullet(int x, int y) {
+		this.x = x;
+		this.y = y;
+		width = 5;
+		height = 10;
+		speed = 10;
 	}
 	
 	public void draw() {
-		
+		Window.out.color("yellow");
+		Window.out.rectangle(x, y, width, height);
 	}
 	
 	public void move() {
-		
+		y += speed;
 	}
 	
-	public boolean checkCollision() {
+	public boolean checkCollision(Player e) {
+		if (Math.abs(x - e.x) <= width / 2 + e.radius / 2 &&
+				Math.abs(y - e.y) <= height / 2 + e.radius / 2) 
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean checkCollision(PlayerBullet e) {
+		if (Math.abs(x - e.x) <= width / 2 + e.width / 2 &&
+				Math.abs(y - e.y) <= height / 2 + e.height / 2) 
+		{
+			return true;
+		}
 		
 		return false;
 	}
