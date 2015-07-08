aa63ed0f95006754259a86ba3aa3059b4bc905fd
LeiFaWing <stevehlee@sbcglobal.net>
2015-07-08 13:33:54 -0700

    Update Main.java

--- Snake/Main.java
+++ Snake/Main.java
@@ -15,12 +15,48 @@ public class Main {
 	static long newHead2x = 250;
 	static long newHead2y = 250;
 	static Food f = new Food();
+	static long foodx = 100;
+	static long foody = 100;
 	static boolean update = false;
 	
 	public static void main (String[] args) {
 		Window.size(800, 800);
 		
 		Firebase server = new Firebase("https://multiplayersnake.firebaseio.com");
+		
+		server.child("foodx").addValueEventListener(new ValueEventListener() {
+
+			@Override
+			public void onCancelled(FirebaseError arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onDataChange(DataSnapshot d) {
+				update = true;
+				foodx = (Long) d.getValue();
+				update = false;
+			}
+			
+		});
+		
+		server.child("foody").addValueEventListener(new ValueEventListener() {
+
+			@Override
+			public void onCancelled(FirebaseError arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onDataChange(DataSnapshot d) {
+				update = true;
+				foody = (Long) d.getValue();
+				update = false;
+			}
+			
+		});
 		
 		server.child("head2x").addValueEventListener(new ValueEventListener() {
 
@@ -64,6 +100,8 @@ public class Main {
 			if (!update) {
 				head2.x = (int) newHead2x;
 				head2.y = (int) newHead2y;
+				f.x = (int) foodx;
+				f.y = (int) foody;
 			}
 			
 			f.draw();
@@ -89,6 +127,8 @@ public class Main {
 			if (head.checkFood(f)) {
 				head.grow();
 				f.reset();
+				server.child("foodx").setValue(f.x);
+				server.child("foody").setValue(f.y);
 			}
 			
 			if (head2.checkBoundaries()) {
@@ -102,6 +142,8 @@ public class Main {
 			if (head2.checkFood(f)) {
 				head2.grow();
 				f.reset();
+				server.child("foodx").setValue(f.x);
+				server.child("foody").setValue(f.y);
 			}
 			
 			Window.frame();
