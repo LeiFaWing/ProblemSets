28ebb2edb74b90a655b438b9ff9a123802508d25
LeiFaWing <stevehlee@sbcglobal.net>
2015-07-09 15:33:14 -0700

    Update Game.java

--- AgarJava/Game.java
+++ AgarJava/Game.java
@@ -9,33 +9,38 @@ import com.firebase.client.FirebaseError;
 
 import apcs.Window;
 
+
+
 public class Game {
+	
+	static Firebase server = new Firebase("https://agarjava.firebaseio.com/");
 
 	public static void main(String[] args) {
 		Window.size(800, 600);
 		Window.setFrameRate(30);
 
-		Firebase server = new Firebase("https://agarjava.firebaseio.com/");
-		
+
+
 		ArrayList<Player> players = new ArrayList<Player>();
-		
+
 		server.child("online").child("steve").setValue(true);
 		server.child("online").child("steve").onDisconnect().setValue(false);
-		
+
 		Player p = new Player("steve");
-		
+
 		server.child("online").addChildEventListener(new ChildEventListener() {
 
 			@Override
 			public void onCancelled(FirebaseError arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void onChildAdded(DataSnapshot data, String _) {
 				String name = data.getKey();
 				players.add(new Player(name));
+				
 			}
 
 			@Override
@@ -52,17 +57,22 @@ public class Game {
 			@Override
 			public void onChildMoved(DataSnapshot arg0, String arg1) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void onChildRemoved(DataSnapshot arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		});
-		
+
+		server.child("stevex").setValue(p.x);
+		server.child("stevey").setValue(p.y);
+		server.child("stever").setValue(p.radius);
+		server.child("steven").setValue(p.name);
+
 		ArrayList <Blob> blobs = new ArrayList <Blob> ();
 
 
@@ -74,10 +84,31 @@ public class Game {
 		while (true) {
 			Window.out.background("white");
 
+			for (int i = 0; i < players.size(); i++) {
+				players.get(i).draw(p.x, p.y);
+				
+				if (p.checkCollision(players.get(i))) {
+					if (p.radius > players.get(i).radius) {
+						p.radius += players.get(i).radius;
+						players.remove(i);
+						i--;
+					}
+					else {
+						p = new Player("steve");
+					}
+				}
+			}
+
 			p.draw();
 
 			for (int i = 0 ; i < blobs.size() ; i++) {
 				blobs.get(i).draw(p.x, p.y);
+				
+				if (p.checkCollision(blobs.get(i))) {
+					blobs.remove(i);
+					p.radius += 1;
+					i--;
+				}
 			}
 
 			if (p.x > 9600) {
@@ -86,6 +117,11 @@ public class Game {
 			}
 
 			p.move();
+			
+
+			server.child("stevex").setValue(p.x);
+			server.child("stevey").setValue(p.y);
+			server.child("stever").setValue(p.radius);
 
 			Window.frame();
 		}
