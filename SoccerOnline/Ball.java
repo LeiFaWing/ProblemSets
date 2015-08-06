ea81a2f296841326a111db01433f89384d45a2a1
LeiFaWing <stevehlee@sbcglobal.net>
2015-08-06 13:14:29 -0700

    Create Ball.java

---
+++ SoccerOnline/Ball.java
@@ -0,0 +1,75 @@
+package soccer;
+
+import apcs.Window;
+
+public class Ball {
+
+	int x, y, radius, dx, dy;
+	String color;
+	
+	public Ball() {
+		x = Window.width() / 2;
+		y = Window.height() / 2;
+		radius = 10;
+		dx = 0;
+		dy = 0;
+		color = "purple";
+	}
+	
+	public void draw() {
+		Window.out.color(color);
+		Window.out.circle(x, y, radius);
+	}
+	
+	public void move() {
+		
+//		dx *= 9 / 10;
+//		dy *= 9 / 10;
+		
+		x += dx;
+		y += dy;
+	}
+	
+	public boolean checkBoundaries() {
+		
+		if (y < radius) {
+			dy = 50;
+			return true;
+		}
+		if (y > Window.height() - radius) {
+			dy = -50;
+			return true;
+		}
+		
+		if (x < radius && y < Window.height() / 2 - 75) {
+			dx = 50;
+			return true;
+		}
+		
+		if (x < radius && y > Window.height() / 2 + 75) {
+			dx = 50;
+			return true;
+		}
+		
+		if (x > Window.width() - radius && y < Window.height() / 2 - 75) {
+			dx = -50;
+			return true;
+		}
+		
+		if (x > Window.width() - radius && y > Window.height() / 2 + 75) {
+			dx = -50;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+}
+
+
+
+
+
+
+
