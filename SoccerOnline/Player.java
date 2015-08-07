59c25ff46856c37130dc56d03fb7ccb337adf774
LeiFaWing <stevehlee@sbcglobal.net>
2015-08-07 11:45:21 -0700

    Update Player.java

--- SoccerOnline/Player.java
+++ SoccerOnline/Player.java
@@ -11,6 +11,8 @@ public class Player {
 	int speed;
 	String name;
 	String color;
+	int sprintTime = 100;
+	boolean tired = false;
 	
 	public Player(int team, String name) {
 		this.team = team;
@@ -55,6 +57,25 @@ public class Player {
 		if (Window.key.pressed("d") && x < Window.width() - radius) {
 			x += speed;
 		}
+		
+		// sprint button is pressed
+		if (Window.key.pressed("space") && !tired) {
+			sprintTime--;
+			speed = 13;
+			
+			if (sprintTime <= 0) {
+				tired = true;
+			}
+		}
+		else {
+			sprintTime++;
+			if (sprintTime > 100) {
+				sprintTime = 100;
+				tired = false;
+			}
+			speed = 10;
+		}
+		
 		
 		
 	}
